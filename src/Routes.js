8e41c9fde8d7c532977395c5d4cf6dee51d31ef0
Hyun <pikatropika@gmail.com>
2021-03-15 18:47:14 +0900

    초기세팅 진짜 최종의 최종 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-// Login 컴포넌트
 import Login from "../src/Pages/Login";
-
-// Main 컴포넌트
-import Main from "../src/Pages/Main";
-
-// MyPage 컴포넌트
 import MyPage from "../src/Pages/MyPage";
-
-// Product 컴포넌트
+import Main from "../src/Pages/Main";
 import Product from "../src/Pages/Product";
 
 class Routes extends React.Component {
