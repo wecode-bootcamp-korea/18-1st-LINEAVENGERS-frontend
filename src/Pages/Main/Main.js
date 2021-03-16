faf41be2a70ef1b39b8d055085b9558c43122549
Hyun <pikatropika@gmail.com>
2021-03-17 00:11:55 +0900

    ADD : Main 페이지 레이아웃 완료

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import CardList from "./CardList/CardList";
 import "./Main.scss";
 
 export class Main extends Component {
@@ -26,7 +27,31 @@ export class Main extends Component {
     return (
       <div className="main">
         <Header />
-        <main className="mainContainer"></main>
+        <main className="mainContainer">
+          <div className="bigSlide">
+            {/* <img
+              className="backgroundImg"
+              alt="Line Freinds Img"
+              src="../Images/LineFriends.jpeg"
+            /> */}
+            <div className="copy">
+              <p className="bigCopy">
+                귀여운 조명이
+                <br />
+                나를 감싸네
+              </p>
+              <p className="smallCopy">BT21 BABY 포터블 무드 램프</p>
+            </div>
+          </div>
+          <article className="hotCategory">
+            <p className="title">카테고리 바로가기</p>
+            <CardList products={this.state.products} />
+          </article>
+          <article className="products">
+            <p className="title">마음껏 둘러보세요</p>
+            <CardList products={this.state.products} />
+          </article>
+        </main>
         <Footer />
       </div>
     );
