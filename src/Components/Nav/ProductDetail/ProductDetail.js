0ffaebf107c18a3d977d54d3acb3c966af7c9222
Joung seoung ok <joungseoungok@Joungui-MacBookPro.local>
2021-03-17 11:03:12 +0900

    PAUSE: 브랜치 이동으로 잠시 커밋

--- src/Components/Nav/ProductDetail/ProductDetail.js
+++ src/Components/Nav/ProductDetail/ProductDetail.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./ProductDetail.scss";
 
 class ProductDetail extends Component {
