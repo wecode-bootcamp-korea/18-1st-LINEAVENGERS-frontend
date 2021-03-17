a0b02eee2b63996400544db66307ec25a9c8cea3
feature/damhobae <d.mo.b@baedamhoui-MacBookAir.local>
2021-03-17 10:19:19 +0900

    ADD: 로그인 레이아웃 완료 및 기능 구현 중

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,83 @@
 import React, { Component } from "react";
+import "./Login.scss";
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      login_id: "",
+      password: "",
+      idCheck: false,
+    };
+  }
 
-export class Login extends Component {
+  handleLoginInfo = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value }, () => {
+      if (name === "login_id" && value !== "") {
+        this.setState({
+          idCheck: regEx.test(value),
+        });
+      }
+    });
+  };
+
   render() {
-    return <div></div>;
+    const { login_id, password, idCheck } = this.state;
+    // console.log({ login_id });
+    // console.log({ password });
+    console.log(this.state.idCheck);
+    return (
+      <form className="login" onSubmit={this.onSubmit}>
+        <div className="loginContainer">
+          <div className="navar_logo">
+            <h1>
+              <a href="https://www.naver.com/">NAVAR</a>
+            </h1>
+          </div>
+          <div className="userBox">
+            <div className="IdBox">
+              <input
+                className="idInput"
+                type="text"
+                placeholder="아이디"
+                name="login_id"
+                value={login_id}
+                onChange={this.handleLoginInfo}
+              />
+              {login_id && idCheck ? "정답" : login_id.length > 0 ? "땡" : ""}
+            </div>
+            <div className="pwBox">
+              <input
+                className="pwInput"
+                type="password"
+                placeholder="비밀번호"
+                name="password"
+                value={password}
+                onKeyPress={this.Validation}
+                onChange={this.handleLoginInfo}
+                // {password.length >= 5 ? "땡" : "ㄴㄴ"}
+              />
+            </div>
+            <div className="loginBox">
+              <button
+                type="submit"
+                className="loginBtn"
+                onClick={this.handleBtn}
+              >
+                로그인
+              </button>
+            </div>
+            <div className="checkBox">
+              <input type="checkBox" />
+              로그인 상태 유지
+              <span>유지IP 보안</span>
+            </div>
+          </div>
+        </div>
+      </form>
+    );
   }
 }
+var regEx = /^[a-zA-Z0-9]*$/;
 
 export default Login;
--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "../src/Pages/Login";
-import MyPage from "../src/Pages/MyPage";
-import Main from "../src/Pages/Main";
-import Product from "../src/Pages/Product";
+import Login from "./Pages/Login/Login";
+import MyPage from "./Pages/MyPage/MyPage";
+import Main from "./Pages/Main/Main";
+import Product from "./Pages/Product/Product";
+import Sigup from "./Pages/Sigup/Sigup";
 
 class Routes extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/login" component={Login} />
+          <Route exact path="/Sigup" component={Sigup} />
           <Route exact path="/main" component={Main} />
           <Route exact path="/mypage" component={MyPage} />
           <Route exact path="/product" component={Product} />
