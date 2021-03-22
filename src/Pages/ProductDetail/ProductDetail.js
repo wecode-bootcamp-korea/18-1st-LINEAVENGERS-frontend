289246f33e0f6833bedf5a85d2cc438506a7bd7c
Joung seoung ok <joungseoungok@Joungui-MacBookPro.local>
2021-03-22 15:09:59 +0900

    PAUSE: 브랜치 변경

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import ContentTop from "./ContentTop/ContentTop";
+import "./ProductDetail.scss";
 
 class ProductDetail extends Component {
   render() {
-    return <div></div>;
+    return (
+      <>
+        <header></header>
+        <div className="productDetailContainer">
+          <ContentTop />
+          <section className="ContentBottom"></section>
+        </div>
+        <footer></footer>
+      </>
+    );
   }
 }
 
