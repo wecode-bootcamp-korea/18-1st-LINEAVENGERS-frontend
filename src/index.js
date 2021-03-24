1a0dd4c0f69f7eaeb95a4baa90eae1ea9e66d925
Hyun <pikatropika@gmail.com>
2021-03-25 00:46:26 +0900

    ADD : 카테고리 메뉴 데이터 Link로 전달

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
-<<<<<<< HEAD
-=======
-import "./Styles/reset.scss";
-import "./Styles/common.scss";
->>>>>>> 317ebc87538d8a41f5c3efa59daecaf672e01124
 import "./Styles/common.scss";
 import "./Styles/reset.scss";
 
