4c4e109dae2930dfb7601a433b452a4f1ca64b73
김상연 <sangyeon-kim@gimsang-yeon-ui-MacBookPro.local>
2021-03-24 18:36:17 +0900

    save

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Product from "./Pages/Product/Product";
 import Signup from "./Pages/Signup/Signup";
 import Payment from "./Pages/Payment/Payment";
-import Login from "./Pages/Login/Login";
 import MyPage from "./Pages/MyPage/MyPage";
 import Main from "./Pages/Main/Main";
-import Product from "./Pages/Product/Product";
 import ShoppingBasket from "./Pages/MyPage/ShoppingBasket/ShoppingBasket";
 import MyPageHeart from "./Pages/MyPage/MyPageHeart";
 import Modal from "./Pages/MyPage/Modal";
@@ -16,11 +13,10 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          {/* <Route exact path="/Login" component={Login} /> */}
           <Route exact path="/Signup" component={Signup} />
-
+          <Route exact path="/mypage" component={MyPage} />
           <Route exact path="/Payment" component={Payment} />
-          <Route exact path="/product" component={Product} />
+          <Route exact path="/main" component={Main} />
           <Route exact path="/shoppingbasket" component={ShoppingBasket} />
           <Route exact path="/mypageheart" component={MyPageHeart} />
           <Route exact path="/modal/" component={Modal} />
